4e3346e79b660adcf8a79272ffd18710f626cddb
wufel <wufelixhk@gmail.com>
2024-05-06 16:58:10 +0800

    initial spring initializr setup

---
+++ src/main/java/com/wufel/acmebank/AcmebankAccountManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wufel.acmebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmebankAccountManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmebankAccountManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wufel/acmebank/AcmebankAccountManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wufel.acmebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmebankAccountManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

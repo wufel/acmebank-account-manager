79630ad8136096ef08855a17bd855abe7a665900
wufel <wufelixhk@gmail.com>
2024-05-09 16:15:53 +0800

    add flyway for db schema and data initialisation

--- src/main/java/com/wufel/acmebank/entity/Account.java
+++ src/main/java/com/wufel/acmebank/entity/Account.java
@@ -1,20 +1,25 @@
 package com.wufel.acmebank.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
 
 import java.math.BigDecimal;
 
-@Entity
+@Entity(name="ACCOUNTS")
 @Data
 @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Account {
 
     //set accountId as string to provide flexibility if accountId number became unnecessarily big or long or decided to switch format in the future
     @Id
+    @Column(name = "account_id")
     private String accountId;
 
+    @Column(name = "balance")
     private BigDecimal balance;
 
 }
